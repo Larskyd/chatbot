0f617737eea40156e465be73d37cc877385eb148
Larskyd <larsky@uia.no>
2025-10-15 14:07:50 +0200

    land

--- models/RecipeModel.php
+++ models/RecipeModel.php
@@ -16,5 +16,24 @@ class RecipeModel
         }
         return [];
     }
+
+    // Hente oppskrifter basert på land 
+    public function getRecipesByArea()
+    {
+        $url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+        $json = file_get_contents($url);
+        if ($json === false) return [];
+        $data = json_decode($json, true);
+
+        //returner array av oppskrifter fra ulike land
+        if (!empty($data['land'])) {
+            return array_map(function($area) {
+                return $area['strArea'];
+            }, $data['land']);
+        }
+        return [];
+    }
 }
+
+
 ?>
