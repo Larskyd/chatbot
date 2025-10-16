186d33d85b1ccb0454ae91e137a318b6fcbf6d4d
Lars Kydland <larsky@uia.no>
2025-10-16 14:02:19 +0200

    Endret fil systemet for style sheet

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,4 +1,4 @@
-<footer style="background:#eee; text-align:center; padding:20px; margin-top:auto;">
+<footer class="footer">
     &copy; <?php echo date('Y'); ?> Mat-chatbot | IS115 | Simen og Lars
 </footer>
 </body>
